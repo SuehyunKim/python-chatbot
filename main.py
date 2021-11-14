9d1705041660a795e7719413659b463cc1ac7b38
SuehyunKim <sue555@naver.com>
2021-11-15 01:22:48 +0900

    username

--- main.py
+++ main.py
@@ -8,6 +8,15 @@ def main():
 
     client = commands.Bot(command_prefix=prefix, intents = intents)
 
+    @client.command(name='ping')
+    async def _ping(ctx):
+        await ctx.send('pong!')
+
+    @client.command(name='이름')
+    async def _name(ctx):
+        name = ctx.message.author.display_name
+        await ctx.send(f'Logged in as {name}')
+
     with open('token.txt', 'r') as f:
         token = f.read()
 
