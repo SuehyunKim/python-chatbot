6e33474ffeec24d5edd3d7e430b25c643ed22b5c
SuehyunKim <sue555@naver.com>
2021-11-16 22:22:44 +0900

    Created Lunch Cog

--- cogs/Homework.py
+++ cogs/Homework.py
@@ -1,8 +1,13 @@
 from discord.ext import commands
+import json
+import random
+
 
 class Homework(commands.Cog):
     def __init__(self, client):
         self.client = client
+        with open("./data/lunch.json", 'r', encoding='utf-8') as f:
+            self.lunchDict = json.load(f)
 
     @commands.Cog.listener()
     async def on_ready(self):
@@ -12,6 +17,14 @@ class Homework(commands.Cog):
     async def _user(self, ctx):
         user = ctx.author.display_name
         await ctx.send(f'Logged in as {user}')
+
+    @commands.command(name="점심추천2")
+    async def recommend_lunch_2(self, ctx, arg1, arg2):
+        categories = [arg1, arg2]
+        category = random.choice(categories)
+        lunch = random.choice(self.lunchDict[category])
+        await ctx.send(f'{arg1}이랑 {arg2}이면.. 오늘 점심은 {lunch} 어떠세요?')
+
 
 def setup(client):
     client.add_cog(Homework(client))
---
+++ cogs/Lunch.py
@@ -0,0 +1,33 @@
+
+from discord.ext import commands
+import json
+import random
+
+
+class Lunch(commands.Cog):
+    def __init__(self, client):
+        self.client = client
+        # 데이터 불러오기
+        with open("./data/lunch.json", 'r', encoding='utf-8') as f:
+            self.lunchDict = json.load(f)
+
+    @commands.Cog.listener()
+    async def on_ready(self):
+        print("Lunch Cog is Ready")
+
+    @commands.command(name="점심추천")
+    async def recommend_lunch(self, ctx, arg=None):
+        if arg == None:
+            # categories : ['한식', '일식', ..]
+            categories = list(self.lunchDict.keys())
+            category = random.choice(categories)  # category : ex) '한식'
+            lunch = random.choice(self.lunchDict[category])  # lunch : ex) 떡볶이
+            await ctx.send(f'오늘 점심은 {category}, 그 중에서 {lunch} 어떠세요?')
+        else:
+            category = arg
+            lunch = random.choice(self.lunchDict[category])
+            await ctx.send(f'오늘 점심은 {lunch} 어떠세요?')
+
+
+def setup(client):
+    client.add_cog(Lunch(client))
